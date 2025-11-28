8935e758daf62e2d2a6808d4c36aa634ad6f1b22
Little72 <sonaliparida07@gmail.com>
2025-11-28 15:53:40 +0530

    ibm coding lru cache

---
+++ src/main/java/com/ibm/code_pair/bookish_spoon/ibmCoding.java
@@ -0,0 +1,55 @@
+package com.ibm.code_pair.bookish_spoon;
+/**
+ * Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+ *
+ * Implement the LRUCache class:
+ *
+ * LRUCache(int capacity) Initializes the LRU cache with positive size capacity.
+ * int get(int key) Return the value of the key if the key exists, otherwise return -1.
+ * void put(int key, int value) Update the value of the key if the key exists.
+ *
+ * Otherwise, add the key-value pair to the cache.
+ * If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+ *
+ * The functions get and put must each run in O(1) average time complexity.
+ * @param args
+ * 
+ * Input["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+ * 
+[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+Output[null, null, null, 1, null, -1, null, -1, 3, 4]
+
+* Explanation
+* LRUCache lRUCache = new LRUCache(2);
+* lRUCache.put(1, 1); // cache is {1=1}
+* lRUCache.put(2, 2); // cache is {1=1, 2=2}
+* lRUCache.get(1);    // return 1
+* lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+* lRUCache.get(2);    // returns -1 (not found)
+* lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+* lRUCache.get(1);    // return -1 (not found)
+* lRUCache.get(3);    // return 3
+* lRUCache.get(4);    // return 4
+ 
+ */
+public class ibmCoding {
+	
+	class Node{
+		int key;
+		int value;
+		Node flag;
+	}
+	Node(int key, int value){
+		this.key=key;
+		this.value=value;
+	}
+    
+	
+	private static int capacity;
+	HashMap<String,Node> map
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
