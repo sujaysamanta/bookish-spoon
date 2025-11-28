1cbc1addc3bbfa2dfedb4f84ed43e45d67e1a82e
Sujay Samanta <sujay.samanta@hashicorp.com>
2025-11-28 14:54:22 +0530

    Initialize Spring Boot project with Gradle setup and basic configuration

---
+++ src/main/java/com/ibm/code_pair/bookish_spoon/BookishSpoonApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.code_pair.bookish_spoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookishSpoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookishSpoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibm/code_pair/bookish_spoon/BookishSpoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.code_pair.bookish_spoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookishSpoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
